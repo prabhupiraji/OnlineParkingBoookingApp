a19b4f9fb0fa8686d60b5eb0479dea2ccb71d865
prabhu <prabhupiraji1998@gmail.com>
2023-09-21 00:57:38 +0530

    changed button color and widtd

--- src/app/contactus/contactus.component.ts
+++ src/app/contactus/contactus.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Usermodel } from '../usermodel';
+import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-contactus',
@@ -6,5 +10,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./contactus.component.css']
 })
 export class ContactusComponent {
+  formData: Usermodel =new Usermodel(); 
+  data: any;
+  // userId: number;
 
+  constructor(private router: Router,  private http: HttpClient,private userService:UserService) { }
+ 
+  RegisterQuery(){
+    this.userService.getCustomerQuery(this.formData).subscribe(data => {
+      console.log(data);
+  
+     this.router.navigate(['/user-home']);
+    },
+    error => console.log(error)
+    );
+  }
 }
